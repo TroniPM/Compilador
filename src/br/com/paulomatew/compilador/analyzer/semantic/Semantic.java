10a2a2ed3c24e330c343c02d591786d02bee1637
Paulo Mateus <paulomatew@gmail.com>
2017-07-27 11:26:06 -0300

    TODO1 completo

--- src/br/com/paulomatew/compilador/analyzer/semantic/Semantic.java
+++ src/br/com/paulomatew/compilador/analyzer/semantic/Semantic.java
@@ -14,11 +14,17 @@ public class Semantic {
     private ArrayList<LexicalToken> tokens = null;
     private ArrayList<Escopo> escopos = null;
 
-    private ArrayList<LexicalToken> identificadoresDeclaracao = null;
-
+    //private ArrayList<LexicalToken> indentifiersDefinitions = null;
+    //private ArrayList<LexicalToken> indentifiersUse = null;
     public void init(ArrayList<LexicalToken> tokens, ArrayList<Escopo> escopos) throws SemanticException {
         this.tokens = tokens;
         this.escopos = escopos;
+
+        //this.indentifiersDefinitions = getIndentifiersDefinitions();
+        //this.indentifiersUse = getIndentifiersUse();
+        //for (LexicalToken in : indentifiersDefinitions) {
+        //    in.print();
+        //}
 
         /*for (LexicalToken in : tokens) {
             System.out.println(in.lexeme);
@@ -26,13 +32,29 @@ public class Semantic {
         LexicalToken flag = checkVariableAlreadyDefinedInScope();
         if (flag != null) {
             throw new SemanticException("Variable already defined in method: "
-                    + flag.lexeme + " at line " + flag.line + ", position " + flag.position);
+                    + flag.lexeme + " at line " + flag.line + ", position " + flag.position
+                    + ", scope " + flag.scope);
         }
 
         flag = checkVariableWasDefinedInScopeBeforeUse();
         if (flag != null) {
             throw new SemanticException("Variable might not have been initialized: "
-                    + flag.lexeme + " at line " + flag.line + ", position " + flag.position);
+                    + flag.lexeme + " at line " + flag.line + ", position " + flag.position
+                    + ", scope " + flag.scope);
+        }
+
+        flag = checkIfIdentifierHasSameNameMethod();
+        if (flag != null) {
+            throw new SemanticException("Variable already defined as method: "
+                    + flag.lexeme + " at line " + flag.line + ", position " + flag.position
+                    + ", scope " + flag.scope);
+        }
+
+        flag = checkIfMethodAlradeyDeclared();
+        if (flag != null) {
+            throw new SemanticException("Method already defined: "
+                    + flag.lexeme + " at line " + flag.line + ", position " + flag.position
+                    + ", scope " + flag.scope);
         }
     }
 
@@ -58,7 +80,6 @@ public class Semantic {
                 }
             }
         }
-
         return null;
     }
 
@@ -116,5 +137,95 @@ public class Semantic {
         }
 
         return checkVariableDefinedInScopeTree(token, escopo.pai);
+    }
+
+    private LexicalToken checkIfIdentifierHasSameNameMethod() {
+        boolean mesmoNome = false;
+        for (int i = 0; i < escopos.size(); i++) {
+            String escopo = escopos.get(i).label;
+            ArrayList<String> token = new ArrayList<>();
+
+            for (int j = 0; j < tokens.size(); j++) {//apenas identificadores
+                LexicalToken atual = tokens.get(j);
+                if (atual.type != 1 || !atual.scope.equals(escopo)) {
+                    continue;
+                }
+                LexicalToken anterior1 = tokens.get(j - 1);
+                LexicalToken anterior2 = tokens.get(j - 2);
+                if (anterior2.type == 24 || anterior1.type != 16 && anterior1.type != 17) {
+                    //int, boolean, function
+                    continue;
+                }
+//aqui
+                for (int x = 0; x < tokens.size(); x++) {
+                    if (tokens.get(x).type == 24) {
+                        LexicalToken identificadorFuncao = tokens.get(x + 2);
+                        if (atual.lexeme.equals(identificadorFuncao.lexeme)) {
+                            return atual;
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private LexicalToken checkIfMethodAlradeyDeclared() {
+        for (int i = 0; i < escopos.size(); i++) {
+            String escopo = escopos.get(i).label;
+            ArrayList<String> token = new ArrayList<>();
+
+            for (int j = 0; j < tokens.size(); j++) {//apenas identificadores
+                LexicalToken atual = tokens.get(j);
+                if (atual.type != 1 || !atual.scope.equals(escopo)) {
+                    continue;
+                }
+                LexicalToken anterior1 = tokens.get(j - 2);
+                if (anterior1.type != 24) {
+                    //function
+                    continue;
+                }
+//aqui
+                for (int x = 0; x < tokens.size(); x++) {
+                    if (tokens.get(x).type == 24 && tokens.get(x + 2) != atual) {
+
+                        LexicalToken identificadorFuncao = tokens.get(x + 2);
+                        if (atual.lexeme.equals(identificadorFuncao.lexeme)) {
+                            return atual;
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private ArrayList<LexicalToken> getIndentifiersUse() {
+        ArrayList<LexicalToken> arr = new ArrayList<>();
+
+        for (int i = 0; i < escopos.size(); i++) {
+            Escopo escopo = escopos.get(i);
+            ArrayList<String> token = new ArrayList<>();
+
+            boolean erro = false;
+
+            for (int j = 0; j < tokens.size(); j++) {//apenas identificadores
+                LexicalToken atual = tokens.get(j);
+                if (atual.type != 1 || !atual.scope.equals(escopo.label)) {
+                    continue;
+                }
+                LexicalToken anterior = tokens.get(j - 1);
+                if (anterior.type == 16 || anterior.type == 17 || anterior.type == 36) {
+                    //INT, BOOLEAN, CALL
+                    continue;
+                }
+
+                arr.add(atual);
+            }
+        }
+
+        return arr;
     }
 }
